4ae1c588d48090d27b1f4003915ceaa9c525973e
Davide Orsucci <39699370+DadeOrsu@users.noreply.github.com>
2023-12-21 12:06:20 +0100

    Add files via upload
    some refactorings

--- MultiSet.hs
+++ MultiSet.hs
@@ -11,11 +11,11 @@ module MultiSet
 
 data MSet a = MS [(a, Int)] deriving (Show)
 
--- Constructor for an empty multiset
+-- |Constructor for an empty multiset
 empty :: MSet a
 empty = MS []
 
--- Add an element to the multiset
+-- |returns a multiset obtained by adding an element v to the multiset
 add :: Eq a => MSet a -> a -> MSet a
 add (MS ms) v = MS (addHelper ms v)
   where
@@ -25,31 +25,27 @@ add (MS ms) v = MS (addHelper ms v)
       | existingV == newV = (existingV, n + 1) : rest
       | otherwise = (existingV, n) : addHelper rest newV
 
--- Count the occurrences of an element in the multiset
+-- |returns the number of occurrences of v in mset
 occs :: Eq a => MSet a -> a -> Int
 occs (MS ms) v = occsHelper ms v
   where
     occsHelper :: Eq a => [(a, Int)] -> a -> Int
     occsHelper [] _ = 0
-    occsHelper ((x, n):rest) target
-      | x == target = n
-      | otherwise = occsHelper rest target
+    occsHelper ((x, n):rest) v
+      | x == v = n
+      | otherwise = occsHelper rest v
 
--- Get a list of all elements in the multiset
+-- |returns a list containing the elements of the multiset
 elems :: MSet a -> [a]
 elems (MS ms) = concatMap (\(x, _) -> [x]) ms
 
--- Check if mset1 is a submultiset of mset2
+-- |returns True if each element of MSet a is also an element
+-- of MSet b with the same multiplicity at least.
 subeq :: Eq a => MSet a -> MSet a -> Bool
-subeq (MS ms1) (MS ms2) = all (\(v, n) -> n <= findMultiplicity v ms2) ms1
-  where
-    findMultiplicity :: Eq a => a -> [(a, Int)] -> Int
-    findMultiplicity _ [] = 0
-    findMultiplicity target ((x, m):rest)
-      | target == x = m
-      | otherwise = findMultiplicity target rest
+subeq (MS ms1) (MS ms2) = all (\(v, n) -> n <= occs (MS ms2) v) ms1
 
--- Union of two multisets
+-- |returns a MSet having all the elements of mset1 and mset2
+-- each with the sum of their multiplicities in mset1 and mset2
 union :: Eq a => Ord a => MSet a -> MSet a -> MSet a
 union (MS ms1) (MS ms2) = MS (unionHelper ms1 ms2)
   where
@@ -61,35 +57,24 @@ union (MS ms1) (MS ms2) = MS (unionHelper ms1 ms2)
       | v1 < v2 = (v1, n1) : unionHelper rest1 ((v2, n2):rest2)
       | otherwise = (v2, n2) : unionHelper ((v1, n1):rest1) rest2
 
--- Equality instance for MSet
+-- |Equality instance for MSet
 instance Eq a => Eq (MSet a) where
+  (==) :: Eq a => MSet a -> MSet a -> Bool
   (==) mset1 mset2 = subeq mset1 mset2 && subeq mset2 mset1
 
--- Foldable instance for MSet
+-- |Foldable instance for MSet
 instance Foldable MSet where
   foldr :: (a -> b -> b) -> b -> MSet a -> b
   foldr f acc (MS ms) = foldr (\(v, _) restAcc -> f v restAcc) acc ms
 
--- Map function for MSet
+-- |Map function for MSet
+-- NOTE:  it is not possible an instance of Functor for MSet
+--        because (according to the documentation) a type f is a 
+--        Functor if it provides a function fmap which, given any types a and b,
+--        lets you apply any function of type (a -> b) to turn an f a into an f b,
+--        preserving the structure of f.
+--        The fact is that mapMSet is designed to be used with MSets, not with
+--        any type a and b.
+--        
 mapMSet :: (a -> b) -> MSet a -> MSet b
 mapMSet f (MS ms) = MS (map (\(v, n) -> (f v, n)) ms)
-
-{-
-  It's not possible to define a lawful instance of Functor for MSet directly using fmap, 
-  because fmap should obey the Functor laws, particularly the preservation of the identity and composition.
-
-  For MSet, the functorial mapping should not only transform the elements of the multiset but also
-  consider the multiplicities associated with each element. 
-
-  Functor laws require that fmap id = id and fmap (g . f) = fmap g . fmap f. However, since MSet is a multiset
-  and not a traditional container type, applying fmap directly would lead to violations of these laws.
-
-  Specifically, fmap id should result in the original multiset, preserving multiplicities, 
-  but applying id to each element may change its multiplicity, violating the law.
-  
-  For example, fmap id (MS [(1, 2), (2, 3)]) would result in MS [(1, 1), (2, 1)], which is not equal to the original multiset.
-
-  Additionally, fmap (g . f) should be equivalent to composing the individual mappings, 
-  but directly applying fmap to MSet would not consider the interplay of multiplicities and might produce incorrect results.
--}
-
--- TestMSet.hs
+++ TestMSet.hs
@@ -2,13 +2,13 @@ import MultiSet
 
 import Data.Char (toLower)
 import Data.List (sort, group)
-import System.IO
+import System.IO ( hPutStrLn, withFile, IOMode(WriteMode) )
 
--- Function to calculate the ciao of a string
+-- |Function to calculate the ciao of a string
 ciao :: String -> String
 ciao = sort . map toLower
 
--- Function to read a text file and create an MSet of ciaos with multiplicities
+-- |Function to read a text file and create an MSet of ciaos with multiplicities
 readMSet :: FilePath -> IO (MSet String)
 readMSet filename = do
   content <- readFile filename
@@ -23,7 +23,7 @@ readMSet filename = do
 -- result <- readMSet filename
 -- putStrLn $ "MSet of ciaos from the file: " ++ show result
 
--- Function to write MSet elements with multiplicities to a file
+-- |Function to write MSet elements with multiplicities to a file
 writeMSet :: Show a => FilePath -> MSet a -> IO ()
 writeMSet filename (MS ms) = withFile filename WriteMode $ \handle -> do
   mapM_ (hPutStrLn handle . formatEntry) ms
@@ -39,11 +39,11 @@ writeMSet filename (MS ms) = withFile filename WriteMode $ \handle -> do
 -- putStrLn $ "Multiset written to file: " ++ outputFilename
 
 
--- Function to check if two multisets have the same elements
+-- |Function to check if two multisets have the same elements
 sameElements :: Eq a => MSet a -> MSet a -> Bool
 sameElements mset1 mset2 = subeq mset1 mset2 && subeq mset2 mset1
 
--- Main function
+-- |Main function
 main :: IO ()
 main = do
   -- a. Load files into corresponding multisets
@@ -55,13 +55,13 @@ main = do
   -- b. Check facts and print comments
   putStrLn "Facts:"
   -- i. Multisets m1 and m4 are not equal, but they have the same elements
-  putStrLn $ "i. Multisets m1 and m4 are not equal: " ++ show (m1 == m4)
-  putStrLn $ "   but they have the same elements: " ++ show (elems m1 == elems m4)
+  putStrLn $ "i.a Multisets m1 and m4 are not equal: " ++ show (m1 /= m4)
+  putStrLn $ "i.b Multisets m1 and m4 have the same elements: " ++ show (elems m1 == elems m4)
   -- ii. Multiset m1 is equal to the union of multisets m2 and m3
-  let unionM2M3 = union m2 m3
+  let unionM2M3 = m2 `union` m3
   putStrLn $ "ii. Multiset m1 is equal to the union of multisets m2 and m3: " ++ show (m1 == unionM2M3)
 
   -- c. Write multisets m1 and m4 to files
   writeMSet "output/anag-out.txt" m1
   writeMSet "output/gana-out.txt" m4
-  putStrLn "Multisets m1 and m4 written to files anag-out.txt and gana-out.txt, respectively."
+  putStrLn "Multisets m1 and m4 written to files output/anag-out.txt and output/gana-out.txt."
